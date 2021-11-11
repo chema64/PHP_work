8921e544800c805c38db1363b2ef0ec565caf90c
Jeremy <chema64@users.noreply.github.com>
2021-11-11 11:31:12 +1000

    multidimensional arrays

--- hello.php
+++ hello.php
@@ -1,13 +1,50 @@
 <?php
 
-$articles = [ 1 => "First post", 3 => "Another post", "Read this!"];
+$count = 3;
+$price = 9.99;
 
-var_dump($articles);
-
-$string_index = [
-  "two" => "First post",
-  "four" => "Another post",
-  "six" => "Read this!"
+$values = [
+  "message" => "Hello world!",
+  "count" => 150,
+  "pi" => 3.14,
+  "status" => false,
+  "result" => null,
+  $count,
+  $price
 ];
 
-var_dump($string_index);
+var_dump($values);
+
+$alice = [
+  "name" => "Alice",
+  "email" => "alice@example.com",
+  "height" => 1.80
+];
+
+$bob = [
+  "name" => "Bob",
+  "email" => "bob@example.com",
+  "height" => 1.67
+];
+
+$carol = [
+  "name" => "Carol",
+  "email" => "carol@example.com",
+  "height" => 1.74
+];
+
+$people = [$alice, $bob, $carol];
+
+var_dump($people);
+
+$alice_email = $people[0]["email"];
+
+var_dump($alice_email);
+
+$articles = [
+  ["title" => "First post", "content" => "This is the first post"],
+  ["title" => "Another post", "content" => "Another post to reaqd here"],
+  ["title" => "Read this!", "content" => "You must read this article!"]
+];
+
+var_dump($articles[1]["title"]);
