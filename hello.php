0d723d6ae5f3d4ef06279c3a74a4a0877fafcb4d
Jeremy <chema64@users.noreply.github.com>
2021-11-11 14:13:09 +1000

    if else comparison operator added

--- hello.php
+++ hello.php
@@ -1,58 +1,11 @@
 <?php
 
-$count = 3;
-$price = 9.99;
+$articles = [];
 
-$values = [
-  "message" => "Hello world!",
-  "count" => 150,
-  "pi" => 3.14,
-  "status" => false,
-  "result" => null,
-  $count,
-  $price
-];
+var_dump(empty($articles));
 
-var_dump($values);
-
-$alice = [
-  "name" => "Alice",
-  "email" => "alice@example.com",
-  "height" => 1.80
-];
-
-$bob = [
-  "name" => "Bob",
-  "email" => "bob@example.com",
-  "height" => 1.67
-];
-
-$carol = [
-  "name" => "Carol",
-  "email" => "carol@example.com",
-  "height" => 1.74
-];
-
-$people = [$alice, $bob, $carol];
-
-var_dump($people);
-
-$alice_email = $people[0]["email"];
-
-var_dump($alice_email);
-
-$articles = [
-  ["title" => "First post", "content" => "This is the first post"],
-  ["title" => "Another post", "content" => "Another post to reaqd here"],
-  ["title" => "Read this!", "content" => "You must read this article!"]
-];
-
-
-
-var_dump($articles[1]["title"]);
-
-$small_articles = ["First post", "Another post", "Read this!"];
-
-foreach ($small_articles as $test) {
-    echo $test, ", test ";
+if (empty($articles))  {
+  echo "This array is empty";
+} else {
+  echo "The array is not empty";
 }
