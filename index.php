54bd9ea6338ad62f4cd16a6bb4a03ee12a6c5c67
Jeremy <chema64@users.noreply.github.com>
2021-11-13 19:42:12 +1000

    Create Dynamic Content: Mixing HTML and PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Dave";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My website</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>Lorem Ipsum</h1>
+<p>Hello. <?php echo $name; ?></p>
+
+
+</body>
+</html>
