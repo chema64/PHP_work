3ea9504472a9a52b3882ed1893a5ad76d8d369fb
Jeremy <chema64@users.noreply.github.com>
2021-11-27 15:22:06 +1000

    Coding Exercise 7: Multiple pages in PHP

---
+++ multiplePages.php
@@ -0,0 +1,20 @@
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Posts</title>
+    </head>
+    <body>
+
+    <h1>Posts</h1>
+
+    <ul>
+        <?php foreach($posts as $id => $title): ?>
+
+            <li><a href="post.php?id=<?= $id;?>"><?= $title; ?></a></li>
+
+        <?php endforeach; ?>
+    </ul>
+
+    </body>
+</html>
